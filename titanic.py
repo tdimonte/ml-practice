7c2cbc63d3468d9fa02f9b740f07875303e1e4c0
tdimonte <tdimonte@luc.edu>
2021-09-14 22:23:47 -0500

    Added titanic.py non ML theory

--- titanic.py
+++ titanic.py
@@ -1,12 +1,15 @@
 #import numpy as np
 import pandas as pd
 
-train = pd.read_csv('train.csv')
-test = pd.read_csv('test.csv')
+# ------------------------------------------------------------------------------- DataFrame 
 
-#remove tables not being considered
+train = pd.read_csv('train.csv')
+
+#remove columns not being considered
 train = train.drop(['Name','SibSp','Parch', 'Ticket', 'Fare', 'Cabin', 'Embarked'],axis=1)
-test = test.drop(['Name','SibSp','Parch', 'Ticket', 'Fare', 'Cabin', 'Embarked'],axis=1)
+
+#add Alive (my custom prediction column calculated to guess if they survived)
+train['Alive'] = '1'
 
 #male/female to 0/1
 for df in [train]:
@@ -14,9 +17,46 @@ for df in [train]:
 
 #fill n/a in as 0
 train['Age'] = train['Age'].fillna(0)
-test['Age'] = test['Age'].fillna(0)
 
-features = ['Pclass','Age','Sex']
-target = 'Survived'
+#turn str data into int data
+train['Pclass'] = pd.to_numeric(train['Pclass'])
+train['Age'] = pd.to_numeric(train['Age'])
+train['Sex'] = pd.to_numeric(train['Sex'])
+train['Alive'] = pd.to_numeric(train['Alive'])
 
-print(train[features].head(3))
+# ------------------------------------------------------------------------------- Prediction
+
+#global variables
+passengers = len(train)
+alive_count = 0
+
+# #create boolean mask of passengers predicted to die
+# #predicted_to_die if 3rd class, non-child, and male
+# predicted_to_die = (train[passengers].Pclass > 2) & (train[passengers].Age > 18) & (train[passengers].Sex > 0)
+
+# #selects rows and updates column
+# train[passengers]['Alive'][predicted_to_die] = 0
+
+#iterate through passenger rows
+for index, row in train.iterrows():
+    if (train.at[index,'Pclass'] > 2):
+        train.at[index,'Alive'] = 0
+
+    if (train.at[index,'Age'] > 18):
+        train.at[index,'Alive'] = 0
+
+    if (train.at[index,'Sex'] > 0):
+        train.at[index,'Alive'] = 0
+
+alive_count = train['Alive'].sum()
+
+for index, row in train.iterrows():
+    if (train.at[index,'Alive'] == 1):
+        print(row['Pclass'],row['Age'],row['Sex'])
+
+print(f'Out of {passengers} passengers. Only {alive_count} survived.')
+
+prediction = pd.DataFrame({'PassengerId':train['PassengerId'],'Alive':train['Alive']})
+
+filename = 'Predicitons.csv'
+prediction.to_csv(filename, index=False)
